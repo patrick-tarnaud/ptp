66598ab66fb7024a006eed46948b2293a0bbd4cb
patrick-tarnaud <patrick.tarnaud@gmail.com>
2018-09-22 19:41:50 +0200

    comments added

--- src/header.js
+++ src/header.js
@@ -1,9 +1,16 @@
-let menu = document.querySelector('div.menu')
-menu.addEventListener('click', (e) => {
+/**
+ * header menu management
+ */
+
+// access to icon hamburger element 
+let hamburger = document.querySelector('.menu a.hamburger')
+console.log(hamburger)
+// when icon hamburger clicked, menu switches in responsive form, if clicked again menu swiches in normal mode (horizonal and ot displayed)
+hamburger.addEventListener('click', (e) => {
+  let menu = document.querySelector('nav div.menu')
   if (menu.classList.contains('responsive')) {
     menu.classList.remove('responsive')
   } else {
     menu.classList.add('responsive')
   }
-  console.log(menu.classList)
 })
