a26bd5b00d3f53640de22038b3a37bc671acdb10
patrick-tarnaud <patrick.tarnaud@gmail.com>
2018-09-24 23:23:52 +0200

    added : app class and div main-container

--- src/index.js
+++ src/index.js
@@ -1,45 +1,16 @@
-import main from "./main.scss";
-import header_scss from "./header.scss"
+// Imports JS
 import header from "./header"
+import App from "./app.js"
+
+// Imports SCSS
+import main from "./main.scss"
 import footer_scss from "./footer.scss"
+import header_scss from "./header.scss"
 
 document.addEventListener('DOMContentLoaded', initialization())
 
-/**
- * Hides all pages
- */
-function hideAllPages() {
-  let pages = document.querySelectorAll('.page')
-  pages.forEach((page) => page.classList.remove('active'))
-}
+let app = null
 
-/**
- * Shows a page from its id
- * 
- * @param {String} pageId 
- */
-function showPage(pageId) {
-  // hides all pages and shows this designed by pageId
-  hideAllPages();
-  let page = document.querySelector(pageId)
-  page.classList.add('active')
-}
-
-/**
- * Document initialization
- */
 function initialization() {
-  // clicks on menu items display pages
-  let menuItems = document.querySelectorAll('nav .menu-item')
-  menuItems.forEach((menuItem) => menuItem.addEventListener('click', (e) => {
-    let page = e.target.getAttribute('href')
-    showPage(page)
-    // if menu is in responsive mode (vertically deployed), collapses it
-    let menu = document.querySelector('.menu')
-    if (menu.classList.contains('responsive')) {
-      menu.classList.remove('responsive')
-    }
-  }))
-  // home page showed firts
-  showPage('#accueil')
+  app = new App()
 }
